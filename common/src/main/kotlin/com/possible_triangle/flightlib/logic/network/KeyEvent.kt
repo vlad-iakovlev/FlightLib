f78278427991dfc884f7c5b045d450dfcae326db
Niklas Widmann <niklas.widmann@zuehlke.com>
2023-06-01 13:31:51 +0200

    Backport to 1.18.2

--- common/src/main/kotlin/com/possible_triangle/flightlib/logic/network/KeyEvent.kt
+++ common/src/main/kotlin/com/possible_triangle/flightlib/logic/network/KeyEvent.kt
@@ -4,7 +4,8 @@ import com.possible_triangle.flightlib.Constants.MOD_ID
 import com.possible_triangle.flightlib.api.FlightKey
 import com.possible_triangle.flightlib.logic.ControlManager
 import net.minecraft.network.FriendlyByteBuf
-import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.ChatType
+import net.minecraft.network.chat.TranslatableComponent
 import net.minecraft.server.level.ServerPlayer
 
 class KeyEvent(val key: FlightKey, val pressed: Boolean, val notify: Boolean = false) {
@@ -12,12 +13,12 @@ class KeyEvent(val key: FlightKey, val pressed: Boolean, val notify: Boolean = f
     companion object {
 
         fun handle(event: KeyEvent, player: ServerPlayer) {
-            if (event.notify) player.sendSystemMessage(
-                Component.translatable(
+            if (event.notify) player.sendMessage(
+                TranslatableComponent(
                     "message.$MOD_ID.control.${event.key.name.lowercase()}",
-                    Component.translatable("message.$MOD_ID.control.${if (event.pressed) "on" else "off"}")
+                    TranslatableComponent("message.$MOD_ID.control.${if (event.pressed) "on" else "off"}")
                 ),
-                true,
+                ChatType.GAME_INFO, player.uuid,
             )
             ControlManager.handle(player, event)
         }
--- forge/src/main/java/com/possible_triangle/flightlib/forge/ForgeEntrypoint.java
+++ forge/src/main/java/com/possible_triangle/flightlib/forge/ForgeEntrypoint.java
@@ -10,8 +10,8 @@ import com.possible_triangle.flightlib.logic.ControlSender;
 import com.possible_triangle.flightlib.logic.JetpackLogic;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
@@ -19,6 +19,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
@@ -39,17 +40,15 @@ public class ForgeEntrypoint {
 
         forgeBus.addListener((PlayerTickEvent event) -> JetpackLogic.INSTANCE.onTick(event.player));
 
-        forgeBus.addListener((InputEvent.Key event) -> ControlSender.INSTANCE.checkKeys());
-
-        forgeBus.addListener((PlayerChangedDimensionEvent event) -> ControlManager.INSTANCE.reset(event.getEntity()));
-        forgeBus.addListener((PlayerLoggedOutEvent event) -> ControlManager.INSTANCE.reset(event.getEntity()));
+        forgeBus.addListener((PlayerChangedDimensionEvent event) -> ControlManager.INSTANCE.reset(event.getPlayer()));
+        forgeBus.addListener((PlayerLoggedOutEvent event) -> ControlManager.INSTANCE.reset(event.getPlayer()));
     }
 
     private void clientInit(IEventBus modBus, IEventBus forgeBus) {
         CommonClass.INSTANCE.clientInit();
 
-        modBus.addListener((RegisterKeyMappingsEvent event) -> ControlManager.INSTANCE.registerKeybinds(event::register));
-        forgeBus.addListener((InputEvent.Key event) -> ControlSender.INSTANCE.checkKeys());
+        modBus.addListener((FMLClientSetupEvent event) -> ControlManager.INSTANCE.registerKeybinds(ClientRegistry::registerKeyBinding));
+        forgeBus.addListener((InputEvent.KeyInputEvent event) -> ControlSender.INSTANCE.checkKeys());
         forgeBus.addListener((PlayerTickEvent event) -> {
             if(event.player instanceof LocalPlayer player) ControlSender.INSTANCE.onTick(player);
         });
