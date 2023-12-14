5fce662bf8d00b5396f7bb42681b14c419ef2143
Niklas Widmann <nik@somethingcatchy.net>
2023-12-14 21:23:04 +0100

    backport latest changes

--- fabric/src/main/kotlin/com/possible_triangle/flightlib/fabric/services/FabricRegistries.kt
+++ fabric/src/main/kotlin/com/possible_triangle/flightlib/fabric/services/FabricRegistries.kt
@@ -1,5 +1,6 @@
 package com.possible_triangle.flightlib.fabric.services
 
+import com.google.common.base.Suppliers
 import com.possible_triangle.flightlib.Constants
 import com.possible_triangle.flightlib.platform.services.IRegistries
 import net.minecraft.core.Registry
@@ -8,7 +9,11 @@ import net.minecraft.sounds.SoundEvent
 
 class FabricRegistries : IRegistries {
 
-    override val swimSpeed = null
+    private val memoizedSwimSpeed = Suppliers.memoize {
+        BuiltInRegistries.ATTRIBUTE[ResourceLocation("porting_lib", "swim_speed")]
+    }
+
+    override val swimSpeed get() = memoizedSwimSpeed.get()
 
     override fun registerSound(name: String): () -> SoundEvent {
         val id = ResourceLocation(Constants.MOD_ID, name)
